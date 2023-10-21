651a195ecf8f249a1f10c6180c125a3febbdee67
maria <cristinabelen97@gmail.com>
2023-10-21 14:41:42 -0500

    second commit

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const pulsar = () => {
   mainMenuElement.classList.toggle('main-menu--show');
 };
 
+
+// div-header
+
+const miDiv = document.getElementById('div-header');
+
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY;
+
+  // Agrega o quita la clase dependiendo de la posición de scroll
+  if (scrollPosition > 50) {
+    miDiv.classList.add('hovered');
+  } else {
+    miDiv.classList.remove('hovered');
+  }
+});
